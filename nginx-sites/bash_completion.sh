9cefea4f865aeb7237715aba0e978e73ebe715c5
Lance Lovette <lance.lovette@gmail.com>
2011-02-14 12:27:52 -0500

    nginx-sites: More robust completion logic; now handles -c option path.

--- nginx-sites/bash_completion.sh
+++ nginx-sites/bash_completion.sh
@@ -2,40 +2,51 @@ _nginx_sites()
 {
 	local cur prev opts cmd i max
 
+	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
 	prev="${COMP_WORDS[COMP_CWORD-1]}"
 	cmd=""
 
-	# The first non-option is the command
+	# Search for a command, skipping options, paths, etc.
 	for (( i=1, max="${#COMP_WORDS[@]}"; i < max; i++ ))
 	do
-		[[ "${COMP_WORDS[$i]}" = -* ]] && continue
+		[[ "${COMP_WORDS[$i]}" =~ ^(list|status|enable|disable)$ ]] || continue
 		cmd="${COMP_WORDS[$i]}"
 		break
 	done
 
-	case "${cmd}" in
-	list)
+	# If we have a command, let it handle the request
+	if [ -n "$cmd" ]; then
 		opts=$(nginx-sites "${cmd}" compgen "$COMP_CWORD" "${COMP_WORDS[@]}" 2> /dev/null)
-		;;
-	status)
-		opts=$(nginx-sites "${cmd}" compgen "$COMP_CWORD" "${COMP_WORDS[@]}" 2> /dev/null)
-		;;
-	enable)
-		opts=$(nginx-sites "${cmd}" compgen "$COMP_CWORD" "${COMP_WORDS[@]}" 2> /dev/null)
-		;;
-	disable)
-		opts=$(nginx-sites "${cmd}" compgen "$COMP_CWORD" "${COMP_WORDS[@]}" 2> /dev/null)
-		;;
-	*)
-		if [[ "${cur}" = -* ]]; then
-			opts="--help --version"
-		else
-			opts="enable disable list status"
-		fi
-		;;
-	esac
+		[ $? -eq 0 ] || return $?
+		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )  
+		return 0
+	fi
 
+	# Filename completions
+	if [ "${prev}" = "-c" ]; then
+		COMPREPLY=( $(compgen -f -- "${cur}") )
+
+		if [ ${#COMPREPLY[@]} -gt 0 ]; then
+			# This turns on -o filenames so a space doesn't get added after the initial
+			# path completion. This option isn't enabled in the 'complete' call because
+			# group names look like path names and those completions get munged if it's on.
+			type compopt &>/dev/null && compopt -o filenames 2>/dev/null || \
+				compgen -f /non-existing-dir/ >/dev/null
+		fi
+
+		return 0
+	fi
+
+	# Expand longopts
+	if [[ "${cur}" = -* ]]; then
+		opts="--help --version"
+		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )  
+		return 0
+	fi
+
+	# If nothing else, show available commands
+	opts="enable disable list status"
 	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )  
 	return 0
 }
